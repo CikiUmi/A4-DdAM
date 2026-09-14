c000e28778c19ae8725afd97104e0cf7799367e7
CikiUmi <escueladhh@gmail.com>
2026-09-14 11:33:08 -0600

    feat(): DatabaseModule usar Hilt con Room

--- GestorNotas/app/src/main/java/com/ddam_a1/gestornotas/di/DatabaseModule.kt
+++ GestorNotas/app/src/main/java/com/ddam_a1/gestornotas/di/DatabaseModule.kt
@@ -1,4 +1,32 @@
 package com.ddam_a1.gestornotas.di
+
+import android.content.Context
+import com.ddam_a1.gestornotas.data.GestorDatabase
+import com.ddam_a1.gestornotas.data.NotaDao
+import com.ddam_a1.gestornotas.data.NotaRepositorio
+import com.ddam_a1.gestornotas.data.NotaRepositorioLocal
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
 // provides para Hilt :dDD
+
+@Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun proveerBaseDatos(@ApplicationContext context: Context): GestorDatabase =
+        GestorDatabase.getDatabase(context)
+
+    @Provides
+    fun proveerDao(db: GestorDatabase): NotaDao = db.notaDao()
+
+    @Provides
+    @Singleton
+    fun proveerRepositorio(dao: NotaDao): NotaRepositorio = NotaRepositorioLocal(dao)
 }
